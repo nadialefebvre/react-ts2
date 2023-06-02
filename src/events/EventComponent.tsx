a484eef5f0395502a249393bb9e9743841c4bcfd
Nadia Lefebvre <95084122+nadialefebvre@users.noreply.github.com>
2023-06-02 15:39:59 +0200

    first part

---
+++ src/events/EventComponent.tsx
@@ -0,0 +1,19 @@
+const EventComponent: React.FC = () => {
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event)
+  }
+
+  const onDragStart = (event: React.DragEvent<HTMLDivElement>) => {
+    console.log(event)
+  }
+  return (
+    <div>
+      <input onChange={onChange} />
+      <div draggable onDragStart={onDragStart}>
+        drag me
+      </div>
+    </div>
+  )
+}
+
+export default EventComponent
---
+++ src/props/Parent.tsx
@@ -0,0 +1,11 @@
+import { Child } from "./Child"
+
+const Parent = () => {
+  return (
+    <Child color="blue" onClick={() => console.log("clicked")}>
+      test
+    </Child>
+  )
+}
+
+export default Parent
